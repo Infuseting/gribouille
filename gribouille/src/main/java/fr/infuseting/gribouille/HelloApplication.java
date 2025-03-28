d9c369ddd644aac64528edcef3a66ef80722f9c4
Infuseting <tuture27100@gmail.com>
2025-03-28 21:55:23 +0100

    [FEATURES] Add onClose features for telling to the user if he really want to close the app

--- gribouille/src/main/java/fr/infuseting/gribouille/HelloApplication.java
+++ gribouille/src/main/java/fr/infuseting/gribouille/HelloApplication.java
@@ -3,9 +3,12 @@ package fr.infuseting.gribouille;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +18,24 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        closeWindow(stage);
+
+    }
+
+    public void closeWindow(Stage stage) {
+        stage.setOnCloseRequest(event -> {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("Confirmation de fermeture");
+            alert.setContentText("Êtes-vous sûr de vouloir fermer la fenêtre ?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                stage.close();
+            } else {
+                event.consume();
+            }
+        });
     }
 
     public static void main(String[] args) {
