abb26356d3aa2acbc15371b8413a2cbc63950e9a
Infuseting <tuture27100@gmail.com>
2025-03-28 22:00:49 +0100

    [FEATURES] We can now draw on canvas

--- gribouille/src/main/java/fr/infuseting/gribouille/HelloApplication.java
+++ gribouille/src/main/java/fr/infuseting/gribouille/HelloApplication.java
@@ -3,14 +3,20 @@ package fr.infuseting.gribouille;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Optional;
 
 public class HelloApplication extends Application {
+
+    private double prevX;
+    private double prevY;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CadreGribouille.fxml"));
@@ -19,7 +25,27 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         closeWindow(stage);
+        Canvas dessin = (Canvas) scene.lookup("Canvas");
+        dessin.setOnMousePressed(this::onMousePressedDrawOnCanvas);
+        dessin.setOnMouseDragged(this::onMouseDraggedDrawOnCanvas);
 
+    }
+
+    private void onMouseDraggedDrawOnCanvas(MouseEvent mouseEvent) {
+        Canvas dessin = (Canvas) mouseEvent.getSource();
+        double x = mouseEvent.getX();
+        double y = mouseEvent.getY();
+        dessin.getGraphicsContext2D().strokeLine(prevX, prevY, x, y);
+        prevX = x;
+        prevY = y;
+    }
+
+    private void onMousePressedDrawOnCanvas(MouseEvent mouseEvent) {
+        Canvas dessin = (Canvas) mouseEvent.getSource();
+        double x = mouseEvent.getX();
+        double y = mouseEvent.getY();
+        prevX = x;
+        prevY = y;
     }
 
     public void closeWindow(Stage stage) {
