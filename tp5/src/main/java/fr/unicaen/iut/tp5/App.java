545191c07b0282c2f87b1c70f822157202703ea7
Infuseting <tuture27100@gmail.com>
2025-05-14 13:00:36 +0200

    [FEATURES] Gestion du controlleur avec gestion des textfield + event lors du changement du ToggleGroup [FEATURES] Initialisation de la grille a partir de la Difficulté choisis. Mise en place des parametres de row and columns + gestion de la taille de chaque case
    (1.3 / 1.4)

---
+++ tp5/src/main/java/fr/unicaen/iut/tp5/App.java
@@ -0,0 +1,25 @@
+package fr.unicaen.iut.tp5;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App   extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("vue.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600 , 600);
+        stage.setTitle("Demineur!");
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- tp5/src/main/java/fr/unicaen/iut/tp5/ControlerDemineur.java
+++ tp5/src/main/java/fr/unicaen/iut/tp5/ControlerDemineur.java
@@ -1,5 +1,61 @@
 package fr.unicaen.iut.tp5;
 
-public class ControlerDemineur {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControlerDemineur implements Initializable {
+
+    @FXML private ToggleGroup difficulty;
+    @FXML private TextField inconnuInput;
+    @FXML private TextField marquesInput;
+    @FXML private GridPane grid;
+
+    private ModeleDemineur modeleDemineur = new ModeleDemineur(0, 0, 0);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        inconnuInput.textProperty().bind(modeleDemineur.nbInconnuesProperty().asString());
+        marquesInput.textProperty().bind(modeleDemineur.nbMarquesProperty().asString());
+
+        difficulty.selectedToggleProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        String selectedDifficulty = newValue.getUserData().toString();
+                        initGrille(selectedDifficulty);
+                    }
+                }
+        );
+
+    }
+
+    private void initGrille(String selectedDifficulty) {
+        grid.getChildren().clear();
+        grid.getRowConstraints().clear();
+        grid.getColumnConstraints().clear();
+
+        int[] data = ModeleDemineur.parseUserData(selectedDifficulty);
+        modeleDemineur = new ModeleDemineur(data[0], data[1], data[2]);
+
+        for (int i = 0; i < data[0]; i++) {
+            RowConstraints row = new RowConstraints();
+            row.setPrefHeight(32);
+            grid.getRowConstraints().add(row);
+        }
+
+        for (int i = 0; i < data[1]; i++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPrefWidth(32);
+            grid.getColumnConstraints().add(column);
+        }
+        System.out.println("Finished creating grid with " + data[0] + " rows and " + data[1] + " columns.");
+
+    }
 }
